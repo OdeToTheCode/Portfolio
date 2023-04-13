d3e0d1f944fbc302ffb45a246cf135b19500b489
OdeToTheCode <119718185+OdeToTheCode@users.noreply.github.com>
2023-04-13 14:21:36 -0500

    github pages

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
   const lastName = "Figueroa"
   const userName = "OdeToTheCode"
 
+  const page = window.location.href.includes("3000") ? window.location.href.split("3000/")[1] : window.location.href.split(".com/")[1]
 
 
   return (
